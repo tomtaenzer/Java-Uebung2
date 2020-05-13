ec3ab95064b65072f7ad8c68b6be3389f429203e
Tom Tänzer <tomtaenzer@gmx.net>
2020-05-13 12:20:47 +0200

    first commit

---
+++ src/UebungDatentypen.java
@@ -0,0 +1,13 @@
+public class UebungDatentypen {
+    public static void main(String[] args){
+
+        String Summe = "Summe";
+        int wert1 = 5;
+        int wert2 = 3;
+        int ergebnis = wert1 + wert2;
+
+        System.out.println(Summe + " : " + ergebnis);
+
+    }
+
+}
---
+++ src/UebungDatentypen2.java
@@ -0,0 +1,19 @@
+public class UebungDatentypen2 {
+    public static void main(String[] args){
+
+    int var1 = 4;
+    int var2 = 5;
+    boolean result;
+
+    result = var1 < var2;
+        System.out.println(result);
+
+    result = var1 > var2;
+        System.out.println(result);
+
+     result = var1 == var2;
+        System.out.println(result);
+
+
+    }
+}
